f0ef3f7297f6eebbed6aa5495288bab2d8d286c4
Zach Richard <idiotzach15@gmail.com>
2014-06-24 18:17:30 -0400

    Some Updates and News.
    Okay so there's just a few changes to the actual code here. The
    important thing is how to use this.
    As Mike and some guys online pointed out to me, the Arduino IDE doesn't
    handle dependencies between libraries very well. They haven't done a ton
    to make it easy to create libraries or use object oriented programming
    yet, especially with the YUN.
    
    For now, the compiler won't complain if the CameraMod.cpp and
    CameraMod.h files are in the arduino/libraries/ directory on your
    machine.
    
    I'm still having trouble getting the functions to work through a
    separate program.ino, but I'll keep you guys updated.
    
    ~Zach

--- CameraMod.h
+++ CameraMod.h
@@ -4,14 +4,22 @@
   Project: Trap Sat: Capitol College
 
   This library will be used to take and store pictures using the Arduino YUN and a USB webcam.
+  To do so, the YUN must have UVC camera support and the program fswebcam installed.
+  If not installed, you can run while the YUN is connected to the internet:
+    $ opkg update
+    $opkg install kmod-video-uvc
+    $opkg install fswebcam
 */
 #ifndef CameraMod_h
 #define CameraMod_h
 // Headers:
-#include "Arduino.h"
+#include <Arduino.h>
+#include <Bridge.h>
+#include <Process.h>
+#include <FileIO.h>
 
 class CameraMod
-{
+{ 
   private:
     String filename;  // Name of the file.
     String location;  // Location to be saved
